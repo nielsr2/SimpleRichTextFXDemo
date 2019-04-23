e4aca823bfd9bc9d5b4b7898d9444b1237aab2eb
niels <nielsr2@gmail.com>
2019-04-23 20:36:31 +0200

    new svg

--- src/zunayedhassan/KBSManager.java
+++ src/zunayedhassan/KBSManager.java
@@ -12,7 +12,8 @@ public class KBSManager {
         this._initializeEvents();
     }
     private SVGPath _init() {
-        String path = "M 100 100 L 300 100 L 200 200 L 0 200 z";
+//        String path = "M 100 100 L 300 100 L 200 200 L 0 200 z";
+        String path = "M224.68,57c-61.16,0-110.8,51.2-110.87,112.36a112.06,112.06,0,0,0,27,73.2,100.45,100.45,0,0,1,21.6,44.64c9.93,47,40.68,58.23,62.29,58.23h0c21.62,0,52.37-11.25,62.29-58.23a100.45,100.45,0,0,1,21.6-44.64,112.06,112.06,0,0,0,27-73.2C335.48,108.2,281,57,224.68,57Z";
         SVGPath svgpath = new SVGPath();
         svgpath.setContent(path);
         return svgpath;
@@ -21,7 +22,7 @@ public class KBSManager {
         this.OUR.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                System.out.println("Hi there! You clicked me!");
+                System.out.println( "Hi there! You clicked me!");
                 OUR.setFill(Color.RED);
             }
         });
