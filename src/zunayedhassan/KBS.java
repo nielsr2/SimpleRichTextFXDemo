1cb49876ba9a3e2e4153b28833d8acd1bfbc99e2
Mikkel Kappel Persson <43847844+MikkelKPersson@users.noreply.github.com>
2019-04-24 12:22:31 +0200

    Ass KBS Class

---
+++ src/zunayedhassan/KBS.java
@@ -0,0 +1,19 @@
+package zunayedhassan;
+
+public class KBS {
+    private String oprSystem;
+    boolean visible = false;
+    int kbsTimesUsed = 0;
+    int tbTimesClicked = 0;
+    int nrOnList;
+    boolean pinned;
+
+    KBS(String oprSystem){
+        this.oprSystem = oprSystem;
+    }
+
+
+
+
+
+}
