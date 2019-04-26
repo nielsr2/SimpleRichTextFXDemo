c7deeba68873b93bc9f4fe65bd59e979580da6ae
Elizabete <eavoti18@student.aau.dk>
2019-04-26 14:26:54 +0200

    new icons

--- src/zunayedhassan/KBS.java
+++ src/zunayedhassan/KBS.java
@@ -1,12 +1,25 @@
 package zunayedhassan;
 
-import javafx.scene.shape.Rectangle;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
 
-public class KBS {
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+
+public class KBS extends Group {
     private String oprSystem;
     private boolean visible = false;
-    protected int kbsTimesUsed = 0;
-    protected int tbTimesClicked = 0;
+    private int kbsTimesUsed = 0;
+    private int tbTimesClicked = 0;
     private int nrOnList;
     private boolean pinned;
     public int KBStype[] = new int[5];
@@ -18,8 +31,36 @@ public class KBS {
         this.oprSystem = oprSystem;
     }
 
-    public Rectangle displayKBS(){
-       return new Rectangle(100,100,100,50);
+    public Group displayKBS(){
+        Group group = new Group();
+
+       // Rectangle backgroundRect = new Rectangle(170,50, Color.LIGHTGREY);
+        Color grColor1 = new Color(0.5, 0.1, 0.9, 0.20);
+        Color grColor2 = new Color(0.1, 0.7, 0.1, 0.20);
+
+        Stop[] stops = new Stop[] { new Stop(0, grColor1), new Stop(1, grColor2)};
+        LinearGradient lg1 = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stops);
+        Rectangle backgroundRect = new Rectangle(170,50);
+        backgroundRect.setFill(lg1);
+
+
+        HBox content = new HBox(5);
+        content.setPadding(new Insets(5,5,5,5));
+
+        ImageView imageView = new ImageView(new Image("zunayedhassan/SimpleRichTextFX/icons/newIconsPNG/CutIcon.png"));
+        imageView.setFitWidth(40);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+
+        Text shortcut = new Text("Ctrl + X");
+        shortcut.setFont(new Font(30));
+
+        content.getChildren().addAll(imageView, shortcut);
+
+        group.getChildren().addAll(backgroundRect, content);
+       return group;
+
     }
 
 
