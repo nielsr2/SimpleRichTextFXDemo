76467959ebcb42f48a195e9f5f1cf91d27ae7c30
Mikkel Kappel Persson <43847844+MikkelKPersson@users.noreply.github.com>
2019-04-30 13:16:16 +0200

    Fixed UI

--- src/zunayedhassan/OurGroup.java
+++ src/zunayedhassan/OurGroup.java
@@ -2,15 +2,13 @@ package zunayedhassan;
 
 import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
-import zunayedhassan.BaseUI;
-import zunayedhassan.KBSManager;
-import zunayedhassan.RootUI;
 
 public class OurGroup extends Group {
     BaseUI root = new RootUI();
     KBSManager km = new KBSManager();
     BorderPane overlayPane = new BorderPane();
     OurGroup(){
+        root.setPrefSize(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT);
         overlayPane.setPrefSize(Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT);
         //overlayPane.setStyle("-fx-border-color: red");
         this.getChildren().addAll(overlayPane,root);
