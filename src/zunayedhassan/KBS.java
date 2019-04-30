793335a388f232bb3b5c018eedefcc6a84829b8c
Elizabete <eavoti18@student.aau.dk>
2019-04-30 11:12:53 +0200

    Merge branch 'Development' of https://github.com/nielsr2/SimpleRichTextFXDemo into Development
    # Conflicts:
    #	src/zunayedhassan/KBS.javaasdf

--- src/zunayedhassan/KBS.java
+++ src/zunayedhassan/KBS.java
@@ -21,8 +21,23 @@ public class KBS {
     private int nrOnList;
     private boolean pinned;
     public int KBStype[] = new int[5];
-    KBS(){
 
+    /** colors for the gradient */
+    // http://www.java2s.com/Tutorials/Java/JavaFX/0110__JavaFX_Gradient_Color.htm
+
+    Color grColorGrey1 = new Color(0.5, 0.5, 0.5, 0.30);
+    Color grColorGrey2 = new Color(0.7, 0.7, 0.7, 0.15);
+
+    Color grColorRed1 = new Color(0.5, 0, 0, 0.70);
+    Color grColorRed2 = new Color(0.5, 0, 0, 0.30);
+
+    Color grColorYellow1 = new Color(0.9, 0.7, 0, 0.70);
+    Color grColorYellow2 = new Color(0.9, 0.7, 0, 0.30);
+
+    Color grColorGreen1 = new Color(0.2, 0.6, 0, 0.70);
+    Color grColorGreen2 = new Color(0.2, 0.6, 0, 0.30);
+
+    KBS(){
     }
 
     KBS(String oprSystem){
@@ -36,44 +51,49 @@ public class KBS {
     }
 
     //  public Rectangle displayKBS(){
-    //     return new Rectangle(100,100,100,50);
+    //  return new Rectangle(100,100,100,50);
     // }
+
+    /** methods for each gradient color gradient */
+
+    public void colorGrey(Color grColorGrey1, Color grColorGrey2) {
+        Stop[] stopsGrey = new Stop[] { new Stop(0, grColorGrey1), new Stop(1, grColorGrey2)};
+        LinearGradient lgGrey = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stopsGrey);
+
+        Rectangle backgroundRectGrey = new Rectangle(170,50);
+        backgroundRectGrey.setFill(lgGrey);
+    }
+
+    public void colorRed(Color grColorRed1, Color grColorRed2){
+        Stop[] stopsRed = new Stop[] { new Stop(0, grColorRed1), new Stop(1, grColorRed2)};
+        LinearGradient lgRed = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stopsRed);
+
+        Rectangle backgroundRectRed = new Rectangle(170,50);
+        backgroundRectRed.setFill(lgRed);
+    }
+
+    public void colorYellow(Color grColorYellow1, Color grColorYellow2){
+        Stop[] stopsYellow = new Stop[] { new Stop(0, grColorYellow1), new Stop(1, grColorYellow2)};
+        LinearGradient lgYellow = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stopsYellow);
+
+        Rectangle backgroundRectYellow = new Rectangle(170,50);
+        backgroundRectYellow.setFill(lgYellow);
+    }
+
+    public void colorGreen(Color grColorGreen1, Color grColorGreen2){
+        Stop[] stopsGreen = new Stop[] { new Stop(0, grColorGreen1), new Stop(1, grColorGreen2)};
+        LinearGradient lgGreen = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stopsGreen);
+
+        Rectangle backgroundRectGreen = new Rectangle(170,50);
+        backgroundRectGreen.setFill(lgGreen);
+    }
+
 
     public Group displayKBS(){
         Group group = new Group();
 
-        // http://www.java2s.com/Tutorials/Java/JavaFX/0110__JavaFX_Gradient_Color.htm
+        // initial rectangle
         // Rectangle backgroundRect = new Rectangle(170,50, Color.LIGHTGREY);
-        Color grColorGrey1 = new Color(0.5, 0.5, 0.5, 0.30);
-        Color grColorGrey2 = new Color(0.7, 0.7, 0.7, 0.15);
-
-        Color grColorRed1 = new Color(0.5, 0, 0, 0.70);
-        Color grColorRed2 = new Color(0.5, 0, 0, 0.30);
-
-        Color grColorYellow1 = new Color(0.9, 0.7, 0, 0.70);
-        Color grColorYellow2 = new Color(0.9, 0.7, 0, 0.30);
-
-        Color grColorGreen1 = new Color(0.2, 0.6, 0, 0.70);
-        Color grColorGreen2 = new Color(0.2, 0.6, 0, 0.30);
-
-
-        // Stop[] stopsRed = new Stop[] { new Stop(0, grColorRed1), new Stop(1, grColorRed2)};
-        // LinearGradient lg1 = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stopsRed);
-
-        // Stop[] stopsYellow = new Stop[] { new Stop(0, grColorYellow1), new Stop(1, grColorYellow2)};
-        // LinearGradient lg2 = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stopsYellow);
-
-        Stop[] stopsGreen = new Stop[] { new Stop(0, grColorGreen1), new Stop(1, grColorGreen2)};
-        LinearGradient lg3 = new LinearGradient(1, 0, 0, 0, true, CycleMethod.NO_CYCLE, stopsGreen);
-
-
-        // Rectangle backgroundRect = new Rectangle(170,50);
-        // Rectangle backgroundRect1 = new Rectangle(170,50);
-        Rectangle backgroundRect2 = new Rectangle(170,50);
-
-        // backgroundRect.setFill(lg1);
-        // backgroundRect1.setFill(lg2);
-        backgroundRect2.setFill(lg3);
 
         HBox content = new HBox(5);
         content.setPadding(new Insets(5,5,5,5));
@@ -89,9 +109,10 @@ public class KBS {
 
         content.getChildren().addAll(imageView, shortcut);
 
-        // group.getChildren().addAll(backgroundRect, content);
-        // group.getChildren().addAll(backgroundRect1, content);
-        group.getChildren().addAll(backgroundRect2, content);
+        // group.getChildren().addAll(backgroundRectGrey, content);
+        // group.getChildren().addAll(backgroundRectRed, content);
+        // group.getChildren().addAll(backgroundRectYellow, content);
+        // group.getChildren().addAll(backgroundRectGreen, content);
 
         return group;
     }
